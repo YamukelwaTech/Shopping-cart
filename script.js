b9c5a0582e539bf023d61a1faf62fd647b783f7c
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-03-02 15:11:32 +0200

    new functions to js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-
 var cartItemsArray = [];
+
+// Load cart items from sessionStorage on page load
+document.addEventListener("DOMContentLoaded", function () {
+  var storedCartItems = sessionStorage.getItem("cartItems");
+  if (storedCartItems) {
+    cartItemsArray = JSON.parse(storedCartItems);
+    updateCartDisplay();
+  }
+});
 
 // Define addToCart in the global scope
 function addToCart(itemName, itemPrice) {
-  var existingItem = cartItemsArray.find(function(item) {
+  var existingItem = cartItemsArray.find(function (item) {
     return item.name === itemName;
   });
 
@@ -13,29 +21,46 @@ function addToCart(itemName, itemPrice) {
     var newItem = {
       name: itemName,
       price: itemPrice,
-      quantity: 1
+      quantity: 1,
     };
     cartItemsArray.push(newItem);
   }
+
+  // Save updated cart items to sessionStorage
+  sessionStorage.setItem("cartItems", JSON.stringify(cartItemsArray));
 
   updateCartDisplay();
 }
 
 function updateCartDisplay() {
-  var cartItemsElement = document.getElementById('cartItems');
-  var totalPriceElement = document.getElementById('totalPrice');
+  var cartItemsElement = document.getElementById("cartItems");
+  var totalPriceElement = document.getElementById("totalPrice");
 
-  cartItemsElement.innerHTML = '';
+  cartItemsElement.innerHTML = "";
 
-  cartItemsArray.forEach(function (item) {
-    var itemElement = document.createElement('div');
-    itemElement.innerHTML = '<p>' + item.name + ' - Qty: ' + item.quantity + ' - R' + (item.price * item.quantity).toFixed(2) + '</p>';
+  cartItemsArray.forEach(function (item, index) {
+    var itemElement = document.createElement("div");
+    itemElement.innerHTML =
+      "<p>" +
+      item.name +
+      " - Qty: " +
+      item.quantity +
+      " - R" +
+      (item.price * item.quantity).toFixed(2) +
+      "</p>";
+
+    // Add a line break or horizontal rule between items (excluding the last one)
+    if (index < cartItemsArray.length - 1) {
+      // You can choose either line break or horizontal rule
+      // itemElement.innerHTML += '<br>';
+      itemElement.innerHTML += "<hr>";
+    }
+
     cartItemsElement.appendChild(itemElement);
   });
 
   var totalPrice = cartItemsArray.reduce(function (total, item) {
-    return total + (item.price * item.quantity);
+    return total + item.price * item.quantity;
   }, 0);
-  totalPriceElement.textContent = 'R' + totalPrice.toFixed(2);
+  totalPriceElement.textContent = "R" + totalPrice.toFixed(2);
 }
-
